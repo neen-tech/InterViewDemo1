0a95db772b0e5050849169ae96df46eeb07822e8
Nitin Tandon <nitin.tandon@hotmail.com>
2025-12-25 21:15:23 +0530

    Initial Commit

---
+++ InterViewDemo/InterViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterViewDemoApp.swift
+//  InterViewDemo
+//
+//  Created by Nitin Tandon on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct InterViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
